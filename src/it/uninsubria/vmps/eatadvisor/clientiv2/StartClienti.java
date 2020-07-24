2b924c0c39660fa866f1fee4be947d3c7e20d9f0
manuelmacaj <49785644+manuelmacaj@users.noreply.github.com>
2020-07-24 16:22:39 +0200

    Added login

--- src/it/uninsubria/vmps/eatadvisor/clientiv2/StartClienti.java
+++ src/it/uninsubria/vmps/eatadvisor/clientiv2/StartClienti.java
@@ -1,12 +1,19 @@
 package it.uninsubria.vmps.eatadvisor.clientiv2;
 
+import it.uninsubria.vmps.eatadvisor.global.Global;
+import it.uninsubria.vmps.eatadvisor.ioutenti.IOUtenti;
+import it.uninsubria.vmps.eatadvisor.ioutenti.Utente;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class StartClienti {
+    private IOUtenti ioUtenti = null;
     public JPanel panelStartClienti;
     private JButton btnAccedi;
     private JButton btnIscriviti;
@@ -17,7 +24,31 @@ public class StartClienti {
     private JLabel lblNickname;
     private JPanel panelLogo;
 
-    public StartClienti() {
+
+    public StartClienti() throws Exception {
+        btnAccedi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Global.utenteLoggato = null;
+                    ioUtenti = new IOUtenti();
+                    System.out.println("Nickname: " + tfNickname.getText());
+                    //System.out.println("Password: " + String.valueOf(tfPassword.getPassword()));
+                    ioUtenti.filtraPerNickname(tfNickname.getText());
+                    ioUtenti.filtraPerPassword(String.valueOf(tfPassword.getPassword()));
+                    System.out.println(ioUtenti.getListaUtenti());
+                    if (ioUtenti.getListaUtenti().size() == 1) {
+                        Global.utenteLoggato = ioUtenti.getListaUtenti().get(0); // prendo l'unico utente nella lista
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Username e/o password errati",
+                                "Attenzione", JOptionPane.PLAIN_MESSAGE);
+                    }
+                } catch (Exception exception) {
+                    System.err.println("IOUTENTI: File " + IOUtenti.FILE_UTENTI + " non trovato.");
+                }
+
+            }
+        });
     }
 
     private void createUIComponents() throws IOException {
---
+++ src/it/uninsubria/vmps/eatadvisor/global/Global.java
@@ -0,0 +1,7 @@
+package it.uninsubria.vmps.eatadvisor.global;
+
+import it.uninsubria.vmps.eatadvisor.ioutenti.Utente;
+
+public class Global {
+    public static Utente utenteLoggato = null;
+}

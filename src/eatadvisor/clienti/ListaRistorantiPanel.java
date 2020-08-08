70db2d12bf29079c3b16e65e0c9d6de49dc55eaf
vellons <vellons@users.noreply.github.com>
2020-08-08 18:12:39 +0200

    ListaRistoranti: updated RistorantePerLista layout

--- src/eatadvisor/clienti/ListaRistorantiPanel.java
+++ src/eatadvisor/clienti/ListaRistorantiPanel.java
@@ -3,20 +3,16 @@ package eatadvisor.clienti;
 import eatadvisor.ioeatadvisor.IOEatAdvisor;
 import eatadvisor.ioeatadvisor.Ristorante;
 
-import javax.print.attribute.standard.JobStateReason;
 import javax.swing.*;
 import java.awt.*;
 
 import static javax.swing.BorderFactory.createEmptyBorder;
 
 public class ListaRistorantiPanel extends JPanel {
-
     public JPanel mainList;
 
     public ListaRistorantiPanel() throws Exception {
         setLayout(new BorderLayout());
-        setBackground(Color.decode("#ff0000"));
-        setForeground(Color.decode("#ff0000"));
 
         // Inizializzazione panel
         mainList = new JPanel(new GridBagLayout());
@@ -33,8 +29,9 @@ public class ListaRistorantiPanel extends JPanel {
 
         // Caricamento ristoranti
         IOEatAdvisor ioEatAdvisor = new IOEatAdvisor();
-        for (Ristorante r : ioEatAdvisor.getListaRistoranti()) {
-            aggiungiRistorante(r.getId(), r.getNomeRistorante(), r.getIndirizzo().toString());
+        for (Ristorante rist : ioEatAdvisor.getListaRistoranti()) {
+            aggiungiRistorante(rist);
+            aggiungiRistorante(rist);
         }
     }
 
@@ -49,9 +46,9 @@ public class ListaRistorantiPanel extends JPanel {
         repaint();
     }
 
-    private void aggiungiRistorante(int id, String nome, String indirizzo) {
+    private void aggiungiRistorante(Ristorante rist) {
         JPanel panel = new JPanel();
-        panel.add(new RistorantePerLista(nome, indirizzo).panelRistorantePerLista);
+        panel.add(new RistorantePerLista(rist).panelRistorantePerLista);
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.weightx = 1;
--- src/eatadvisor/clienti/RistorantePerLista.java
+++ src/eatadvisor/clienti/RistorantePerLista.java
@@ -1,14 +1,21 @@
 package eatadvisor.clienti;
 
+import eatadvisor.ioeatadvisor.Ristorante;
+
 import javax.swing.*;
 
-public class RistorantePerLista extends JPanel{
+public class RistorantePerLista extends JPanel {
     public JPanel panelRistorantePerLista;
     private JLabel lblNome;
     private JLabel lblIndirizzo;
+    private JLabel lblStelle;
+    private JLabel lblValutazioni;
 
-    public RistorantePerLista(String nome, String indirizzo) {
-        lblNome.setText(nome);
-        lblIndirizzo.setText(indirizzo);
+
+    public RistorantePerLista(Ristorante ristorante) {
+        lblNome.setText(ristorante.getNomeRistorante());
+        lblIndirizzo.setText(ristorante.getIndirizzo().toString());
+        lblStelle.setText(String.valueOf(ristorante.getRecensioniValutazioneMedia()));
+        lblValutazioni.setText(String.valueOf(ristorante.getRecensioni().size()) + " valutazioni");
     }
 }

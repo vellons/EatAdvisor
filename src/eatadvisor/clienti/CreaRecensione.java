546da38453c7670f2b10a0aaae2974e49336a065
manuelmacaj <49785644+manuelmacaj@users.noreply.github.com>
2020-08-22 22:36:59 +0200

    Now the counter works correctly

--- src/eatadvisor/clienti/CreaRecensione.java
+++ src/eatadvisor/clienti/CreaRecensione.java
@@ -7,11 +7,10 @@ import eatadvisor.ioeatadvisor.Ristorante;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -26,10 +25,9 @@ public class CreaRecensione {
     private JPanel panelLogo;
     private JButton btnAddCommento;
     private JLabel lbCounterCharacter;
-    private int count = 0;
 
     public CreaRecensione(Ristorante rist) throws Exception {
-        lbCounterCharacter.setText("Caratteri: " + count + "/256");
+        lbCounterCharacter.setText("Caratteri: 0/256");
 
         btnAddCommento.addActionListener(new ActionListener() {
             @Override
@@ -64,23 +62,24 @@ public class CreaRecensione {
 
             }
         });
-        txtCommento.addKeyListener(new KeyAdapter() {
+        txtCommento.getDocument().addDocumentListener(new DocumentListener() {
             @Override
-            public void keyPressed(KeyEvent e) {
-                super.keyPressed(e);
+            public void insertUpdate(DocumentEvent e) {
+                update();
+            }
 
-                if (e.getKeyChar() == KeyEvent.VK_BACK_SPACE) { // se pigio il tasto backspace, entro nell'if
-                    if (txtCommento.getText() == "") { //qualora un utente decidesse di selezionare tutto il commento e cancellarlo tamite il backspace, azzeero il contatore
-                        count = 0;
-                        lbCounterCharacter.setText("Caratteri: " + count + "/256");
-                    } else { //se seleziono una parte del testo
-                        count = txtCommento.getText().length();
-                        lbCounterCharacter.setText("Caratteri: " + count + "/256");
-                    }
-                } else {
-                    count = txtCommento.getText().length() + 1; // incremento il contatore, da momento che sto aggiungendo caratteri
-                    lbCounterCharacter.setText("Caratteri: " + count + "/256");
-                }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                update();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                update();
+            }
+
+            public void update() {
+                lbCounterCharacter.setText("Caratteri: " + txtCommento.getText().length() + "/256");
             }
         });
     }
@@ -108,6 +107,5 @@ public class CreaRecensione {
         position.put(5, new JLabel("5"));
         // Set the label to be drawn
         sliValutazione.setLabelTable(position);
-
     }
 }

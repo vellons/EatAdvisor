181d571a33e857374c49cfe4145b631a4634a6d0
Saladone <topodidi00@gmail.com>
2020-08-25 19:58:22 +0200

    Consegna speciale!

--- src/eatadvisor/ristoratori/DashboardRistoratori.java
+++ src/eatadvisor/ristoratori/DashboardRistoratori.java
@@ -7,13 +7,78 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
+/**
+ * La classe DashboardRistoratori permette il caricamento della dashboard per il
+ * lato del ristoratore insieme alle sue relative funzioni
+ *
+ * @author Mahdi Said
+ */
+
 public class DashboardRistoratori extends JFrame {
+
+    /**
+     * <code>panelDashboardRistoratori</code> è un pannello Swing che compone
+     * l'interfaccia grafica, nella fattispecie la dahboard per i ristoratori
+     * <p>
+     * è dichiarato <strong>public</strong> in quanto l'attributo è utilizzabile all'esterno della classe
+     */
+
     public JPanel panelDashboardRistoratori;
+
+    /**
+     * <code>panelLogo</code> è un pannello Swing che compone
+     * l'interfaccia grafica, nella fattispecie il logo dell'appicazione.
+     * <p>
+     * è dichiarato <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     */
+
     private JPanel panelLogo;
+
+    /**
+     * <code>panelListaRistoranti</code> è un pannello Swing che compone
+     * l'interfaccia grafica, nella fattispecie la parte che si occupa di
+     * ricevere dati dai ristoranti
+     * <p>
+     * è dichiarato <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     */
+
     private JPanel panelListaRistoranti;
+
+    /**
+     * <code>panelFunzioni</code> è un pannello Swing che compone
+     * l'interfaccia grafica, nella fattispecie la parte con il bottone di aggiunta.
+     * <p>
+     * è dichiarato <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     */
+
     private JPanel panelFunzioni;
+
+    /**
+     * <code>btnAggiungi</code> è un bottone Swing che attiva la procedura
+     * di aggiunta di un ristorante di proprietà del ristoratore loggato
+     * è dichiarato <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     */
+
     private JButton btnAggiungi;
+
+    /**
+     * <code>registrazioneFrame</code> è una cornice Swing attivata nel momento nel
+     * quale un nuovo ristorante viene creato
+     * @see RegistrazioneRistorante
+     * <p>
+     * è dichiarata <strong>public</strong> in quanto l'attributo è utilizzabile all'esterno della classe
+     * è dichiarata <strong>static</strong> così da non doverla istanziare creando un oggetto
+     */
+
+
     public static JFrame registrazioneFrame = new JFrame("EatAdvisor Ristoratori - Registrazione Ristorante");
+
+    /**
+     * Main della classe
+     *
+     * @throws Exception è utilizzata quando non si sa che tipo di eccezione potrebbe
+     * essere sollevata durante l'esecuzione del programma
+     */
 
     public DashboardRistoratori() throws Exception {
         btnAggiungi.addActionListener(new ActionListener() {
@@ -32,6 +97,15 @@ public class DashboardRistoratori extends JFrame {
             }
         });
     }
+
+    /**
+     * <code>createUIComponents</code> è una procedura per impostare la grafica
+     * quando viene caricato il frame
+     * è dichiarato <strong>void</strong> in quanto non restituisce alcun valore
+     *
+     * @throws Exception è utilizzata quando non si sa che tipo di eccezione potrebbe
+     * essere sollevata durante l'esecuzione del programma
+     * */
 
     private void createUIComponents() throws Exception {
         panelLogo = new JPanel();
--- src/eatadvisor/ristoratori/MioRistorantePerLista.java
+++ src/eatadvisor/ristoratori/MioRistorantePerLista.java
@@ -7,14 +7,69 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
+/**
+ * La classe MioRistorantePerLista permette il caricamento dei singoli ristotanti
+ * da caricare sulla dashboard
+ *
+ * @author Mahdi Said
+ */
+
 public class MioRistorantePerLista {
+
+    /**
+     * <code>dettaglioFrame</code> è una cornice Swing attivata nel momento nel
+     * quale si vuole visualizzare le informazioni dettagliate di un singolo
+     * ristorante
+     * @see DettaglioMioRistorante
+     * <p>
+     * è dichiarata <strong>public</strong> in quanto l'attributo è utilizzabile all'esterno della classe
+     * è dichiarata <strong>static</strong> così da non doverla istanziare creando un oggetto
+     */
+
     public static JFrame dettaglioFrame = new JFrame("EatAdvisor Ristoratore - Dettaglio mio ristorante");
+
+    /**
+     * <code>panelMioRistorantePerLista</code> è un pannello Swing che compone
+     * l'interfaccia grafica, nella fattispecie uno dei singoli ristoranti
+     * caricati sulla dashboard
+     * <p>
+     * è dichiarato <strong>public</strong> in quanto l'attributo è utilizzabile all'esterno della classe
+     */
+
     public JPanel panelMioRistorantePerLista;
+
+    /**
+     * <code>lblNome</code> è un'etichetta Swing dedicata al campo nome
+     * è dichiarata <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     * <p>
+     * <code>lblIndirizzo</code> è un'etichetta Swing dedicata al campo indirizzo
+     * è dichiarata <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     * <p>
+     * <code>lblStella</code> è un'etichetta Swing dedicata al campo stelle
+     * è dichiarata <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     * <p>
+     * <code>lblValutazioni</code> è un'etichetta Swing dedicata al campo valutazioni
+     * è dichiarata <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     */
+
     private JLabel lblNome;
     private JLabel lblIndirizzo;
     private JLabel lblStelle;
     private JLabel lblValutazioni;
+
+    /**
+     * <code>btnDettaglio</code> è un bottone Swing che attiva visualizzazione
+     * delle informazioni dettagliate di un ristorante a scelta
+     * è dichiarato <strong>private</strong> in quanto l'attributo è utilizzabile all'interno della classe
+     */
+
     private JButton btnDettaglio;
+
+    /**
+     * Main della classe
+     *
+     * @param ristorante insieme di dati relativi al ristorante da visualizzare
+     */
 
     public MioRistorantePerLista(Ristorante ristorante) {
         DecimalFormat dec = new DecimalFormat("#0.00");
